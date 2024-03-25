638d9a6b1532dc76c446d8a48dac312162892c87
Vibu Roshin E V <viburoshin57@gmail.com>
2024-03-25 12:27:17 +0530

    Update ResultTable.jsx

--- src/components/ResultTable.jsx
+++ src/components/ResultTable.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 
 const ResultTable = () => {
-  return <div>Result</div>;
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest Year</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+    </table>
+  );
 };
-ResultTable;
 
-export default Result;
-ResultTable;
+export default ResultTable;
